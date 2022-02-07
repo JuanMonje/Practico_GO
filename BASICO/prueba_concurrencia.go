4d3867652b1747c683a28278970d728f8a36bb7a
Juan Monje <sebastianmoca123@hotmail.com>
2022-02-07 15:38:03 -0500

    Se mejora el ejemplo de concurrencia en prueba_concurrencia.go

--- BASICO/prueba_concurrencia.go
+++ BASICO/prueba_concurrencia.go
@@ -1,6 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"sync"
+)
+
+/* var wg sync.WaitGroup
+
+func mostrar0() {
+	for f := 1; f <= 1000; f++ {
+		fmt.Print("0-")
+	}
+	wg.Done()
+}
+
+func mostrar1() {
+	for f := 1; f <= 1000; f++ {
+		fmt.Print("1-")
+	}
+	wg.Done()
+}
+
+func main() {
+	wg.Add(2)
+	go mostrar0()
+	go mostrar1()
+	wg.Wait()
+} */
 
 func mostrar0() {
 	for f := 1; f <= 1000; f++ {
@@ -15,25 +41,6 @@ func mostrar1() {
 }
 
 func main() {
-	go mostrar0()
-	go mostrar1()
-	var continua string
-	fmt.Scan(&continua)
-}
-
-/* func mostrar0() {
-	for f := 1; f <= 1000; f++ {
-		fmt.Print("0-")
-	}
-}
-
-func mostrar1() {
-	for f := 1; f <= 1000; f++ {
-		fmt.Print("1-")
-	}
-}
-
-func main() {
 	mostrar0()
 	mostrar1()
-} */
+}
